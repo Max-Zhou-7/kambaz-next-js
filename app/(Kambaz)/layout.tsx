9a8d97f6e3579fea802a235b2d24127fb536b901
Max-Zhou-7 <ziluzhou7@gmail.com>
2025-09-22 10:41:54 -0400

    change id to cid in [cid] layout.tsx

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from "react";
-import KambazNavigation from "./Navigation";
-export default function KambazLayout({ children }: Readonly<{ children: ReactNode }>) {
+import CourseNavigation from "./Navigation";
+export default async function CoursesLayout(
+  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+ const { cid } = await params;
  return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="200">  <KambazNavigation /> </td>
-         <td valign="top" width="100%"> {children}           </td>
-       </tr>
-     </tbody>
-   </table>
+   <div id="wd-courses">
+     <h2>Courses {cid}</h2>
+     <hr />
+     <table>
+       <tbody>
+         <tr>
+           <td valign="top" width="200"> <CourseNavigation /> </td>
+           <td valign="top" width="100%"> {children} </td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
 );}
