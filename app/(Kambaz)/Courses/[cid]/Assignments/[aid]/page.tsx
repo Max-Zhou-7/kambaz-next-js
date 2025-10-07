7fefe70e81752bd2d56c5a0938f3470a552ad937
Max-Zhou-7 <ziluzhou7@gmail.com>
2025-10-07 12:32:57 -0400

    a2 CSS and Bootstrap

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -8,13 +8,13 @@ import { FormControl,
           InputGroup
 
  } from "react-bootstrap";
-import { InputGroupText } from "reactstrap";
+
 import { SlCalender } from "react-icons/sl";
 export default function AssignmentEditor() {
   return (
     <div id="wd-assignments-editor">
       <label htmlFor="wd-name"><b>Assignment Name</b></label><br/><br/>
-      <FormControl id="wd-name" defaultValue="A1" className="mb-3" /><br />
+      <FormControl id="wd-name" defaultValue="A1" className="mb-3" />
       <Row className="mb-3" controlId="textarea2">
         <Col sm={20}>
 <FormControl   as="textarea" style={{ height: "100px" }} 
@@ -36,7 +36,9 @@ id="wd-description"
 
 
             <Row className="mb-3" controlId="AssignmentGroup">
-            <FormLabel column sm ={2} htmlFor="wd-group">Assignment Group</FormLabel>
+              <Col sm ={2}>
+            <FormLabel htmlFor="wd-group">Assignment Group</FormLabel>
+            </Col>
      <Col sm={10}>
             <FormSelect id="wd-group">
               <option value= "ASSIGNMENTS">ASSIGNMENTS</option>
@@ -57,29 +59,39 @@ id="wd-description"
           </FormSelect>
         </Col>
       </Row>
-              <FormLabel column sm={2} htmlFor="wd-submission-type">Submission Type</FormLabel>
+      <Row className="mb-3 ">
+<Col sm ={2}>
+              <FormLabel column sm={10} htmlFor="wd-submission-type">Submission Type</FormLabel>
+              </Col>
+              <Col sm={10}>
       <div className="border rounded p-3 mb-3">
-      <Row className="mb-3">
+
 
         <Col sm={10}>
           <FormSelect id="wd-submission-type">
             <option value="ONLINE">Online</option>
           </FormSelect>
         </Col>
-      </Row>
 
-      <FormLabel className="mt-3 mb-2">Online Entry Options</FormLabel>
+
+      <FormLabel className="mt-3 mb-2"><b>Online Entry Options</b></FormLabel>
       <FormCheck type="checkbox" id="wd-text-entry" label="Text Entry" className="mb-2" />
       <FormCheck type="checkbox" id="wd-website-url" label="Website URL" className="mb-2" />
       <FormCheck type="checkbox" id="wd-media-recordings" label="Media Recordings" className="mb-2" />
       <FormCheck type="checkbox" id="wd-student-annotation" label="Student Annotation" className="mb-2" />
       <FormCheck type="checkbox" id="wd-file-upload" label="File Uploads" className="mb-2" />
 </div>
+</Col>
+</Row>
       </Col>
       </Row>
-              <FormLabel column sm={2} htmlFor="wd-assign-to">Assign to</FormLabel>
+      <Row className="mb-3 ">
+<Col sm ={2}>
+              <FormLabel htmlFor="wd-assign-to">Assign</FormLabel>
+                            </Col>
+              <Col sm={10}>
     <div className="border rounded p-3 mb-3">
-      <FormLabel className="mt-3 mb-2">Assign</FormLabel>
+      <FormLabel className="mt-3 mb-2"><b>Assign to</b></FormLabel>
       <Row className="mb-3">
 
         <Col sm={10}>
@@ -88,7 +100,7 @@ id="wd-description"
       </Row>
 
 
-<FormLabel className="mt-3 mb-2">Due</FormLabel>
+<FormLabel className="mt-3 mb-2"><b>Due</b></FormLabel>
       <Row className="mb-3">
 
         <Col sm={10}>
@@ -102,7 +114,7 @@ id="wd-description"
 
       <Row className="mb-3">
     <Col sm={6}>
-    <FormLabel className="mb-2">Available from</FormLabel>
+    <FormLabel className="mb-2"><b>Available from</b></FormLabel>
         
           <InputGroup>
             <FormControl  id="wd-available-from" defaultValue="2024-05-06" />
@@ -111,7 +123,7 @@ id="wd-description"
         </Col>
 
     <Col sm={6}>
-      <FormLabel className="mb-2">Until</FormLabel>
+      <FormLabel className="mb-2"><b>Until</b></FormLabel>
     
           <InputGroup>
   
@@ -121,6 +133,8 @@ id="wd-description"
         </Col>
       </Row>
 </div>
+</Col>
+</Row>
       <div style={{ textAlign: "right" }}>
         <button id="wd-cancel" className="btn btn-secondary me-2">Cancel</button>
         <button id="wd-save" className="btn btn-danger">Save</button>
